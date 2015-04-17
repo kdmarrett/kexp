acbf0b23762c6c8c538f5a655531b3d9372bb74e
KARL D MARRETT <kdmarret@u.washington.edu>
2015-04-17 11:41:20 -0700

    running subjects

--- clean.sh
+++ clean.sh
@@ -3,6 +3,8 @@
 rm expCode/*.pyc
 # prevents from running on a past version
 
+rm -r Data/dummy*
+
 # Matlab has been know to sometimes not overwrite wav files of the same name
 rm Stims/16000/*.wav
 rm Stims/24414/*.wav
